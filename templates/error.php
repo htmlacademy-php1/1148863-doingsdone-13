6c2ada3e425c55476c25d86ff16ebab3ab91a457
tomhell27 <53575983+tomhell27@users.noreply.github.com>
2021-08-26 12:34:35 +0300

    Проверяет корректность заполнения формы и добавляет новую задачу

--- templates/error.php
+++ templates/error.php
@@ -1,6 +1,9 @@
 <section class="content__side"></section>
 
 <main class="content__main">
-    <h2 class="content__main-heading">Ошибка подключения:</h2>
-    <p><?= $error; ?></p>
-</main>
+    <h2 class="content__main-heading">Внимание! Произошла ошибка!:</h2>
+    <ul>
+<?php foreach($error_page as $error):?>
+    <li class="content__main-heading"><?=$error?></li>
+<?php endforeach; ?>
+</ul>
